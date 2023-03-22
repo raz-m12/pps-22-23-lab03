b820b749b84be64cd222acc6925b116638f59c74
Razvan Vasile <razvanflorian.vasile@studio.unibo.it>
2023-03-22 22:40:16 +0100

    finish up to task 2

---
+++ src/main/scala/u03/homework/lab03.scala
@@ -0,0 +1,71 @@
+package u03.homework
+
+import scala.annotation.tailrec
+
+
+object Part1:
+
+  import u03.Lists.List
+  import u03.Lists.List.*
+  import u02.Optionals.Option.*
+  import u02.Optionals.Option
+
+  def drop[A](l: List[A], n: Int): List[A] = (l, n) match
+    case (Cons(_, t), 1) => t
+    case (Cons(_, Nil()), _) => Nil()
+    case (Cons(h, t), _) => Cons(h, drop(t, n-1))
+    case (Nil(), _) => Nil()
+
+  def append[A](left: List[A], right: List[A]): List[A] = (left, right) match
+    case (Cons(h, Nil()), r) => Cons(h, r)
+    case (Cons(h, t), r) => Cons(h, append(t, r))
+    case (Nil(), r) => r
+
+  def flatMap[A, B](l: List[A])(f: A => List[B]): List[B] = l match
+    case Nil() => Nil()
+    case Cons(h, Nil()) => f(h)
+    case Cons(h, t) => f(h) match
+      case r => append(r, flatMap(t)(f))
+
+  def mapList[A, B](l: List[A])(mapper: A => B): List[B] =
+    flatMap(l)(el => Cons(mapper(el), Nil()))
+
+  def filterList[A](l: List[A])(predicate: A => Boolean): List[A] =
+    flatMap(l)(el => predicate(el) match
+      case true => Cons(el, Nil())
+      case false => Nil()
+    )
+
+  private def value(v: Option[Int]): Int = v match
+    case None() => Int.MinValue
+    case Some(v) => v
+
+  def max(l: List[Int]): Option[Int] = l match
+    case Nil() => None[Int]()
+    case Cons(h, t) => value(max(t)) match
+      case v if h > v => Some(h)
+      case v => Some(v)
+
+object Part1MoreOnLists:
+
+  import Part1.flatMap
+  import u03.Lists.List
+  import u03.Lists.List.*
+  import u02.Modules.Person.*
+  import u02.Modules.Person
+
+  import math.Fractional.Implicits.infixFractionalOps
+  import math.Integral.Implicits.infixIntegralOps
+  import math.Numeric.Implicits.infixNumericOps
+  def getCourses(l: List[Person]): List[String] =
+    flatMap(l) { case Teacher(_, course) => Cons(course, Nil()); case _ => Nil() }
+
+  @tailrec
+  def foldLeft[A, B](l: List[A])(acc: B)(op: (B, A) => B): B = l match
+    case Cons(h, Nil()) => op(acc, h)
+    case Cons(h, t) => foldLeft(t)(op(acc, h))(op)
+
+  def foldRight[A, B](l: List[A])(acc: B)(op: (A, B) => B): B = l match
+    case Cons(h, Nil()) => foldLeft(Cons(h, Nil()))(acc)((b, a) => op(a, b))
+    case Cons(h, t) => foldRight(Cons(h, Nil()))(foldRight(t)(acc)(op))(op)
+
---
+++ src/test/scala/u03/homework/Lab03Test.scala
@@ -0,0 +1,129 @@
+package u03.homework
+
+import org.junit.Test
+import org.junit.Assert.{assertEquals, assertNull}
+import org.junit.Before
+
+import u03.homework.Part1.*
+import u03.Lists.List.*
+import u03.Lists.List
+case object Part1ListTests:
+  import u02.Optionals.Option.Some
+  import u02.Optionals.Option.None
+  import u02.Optionals.Option
+
+  val list: List[Int] = Cons(10, Cons(20, Cons(30, Nil())))
+  class BehaviorTests:
+    @Test
+    def dropFirst() =
+      val expected = Cons(20, Cons(30, Nil()))
+      assertEquals(expected, drop(list, 1))
+
+    @Test
+    def dropRecursive() =
+      val expected = Cons(10, Cons(30, Nil()))
+      assertEquals(expected, drop(list, 2))
+
+    @Test
+    def appendOneElementLists() =
+      val expected = Cons(10, Cons(20, Nil()))
+      assertEquals(expected, append(Cons(10, Nil()), Cons(20, Nil())))
+
+    @Test
+    def appendArbitraryLengthLists() =
+      val toAppend = Cons(40, Cons(50, Nil()))
+      val expected = Cons(10, Cons(20, Cons(30, Cons(40, Cons(50, Nil())))))
+      assertEquals(expected, append(list, toAppend))
+
+    @Test
+    def flatMapIncrementByOneSimpleList() =
+      val expected = Cons(11, Nil())
+      assertEquals(expected, flatMap(Cons(10, Nil()))(v => Cons(v+1, Nil())))
+    @Test
+    def flatMapSimple() =
+      val expected = Cons(11, Cons(21, Cons(31, Nil())))
+      assertEquals(expected, flatMap(list)(v => Cons(v+1, Nil())))
+
+    @Test
+    def flatMapComplex() =
+      val expected = Cons(11, Cons(12, Cons(21, Cons(22, Cons(31, Cons(32, Nil()))))))
+      assertEquals(expected, flatMap(list)(v => Cons(v + 1, Cons(v + 2, Nil()))))
+
+    @Test
+    def mapSimple() =
+      val expected = Cons(true, Cons(false, Cons(false, Nil())))
+      assertEquals(expected, mapList(list)(v => v < 15))
+
+    @Test
+    def filterSimpleList() =
+      val expected = Cons(10, Cons(20, Nil()))
+      assertEquals(expected, filterList(list)(v => v < 25))
+
+    @Test
+    def maxComplexCase() =
+      val expected = Some(25)
+      assertEquals(expected, max(Cons(10, Cons(25, Cons(20, Nil())))))
+
+    @Test
+    def maxNoneCase() =
+      val expected = None()
+      assertEquals(expected, max(Nil()))
+
+  class EdgeCaseTests:
+    val expectedNil = Nil()
+    @Test
+    def dropIndexOutOfBounds() =
+      val expected = Cons(10, Cons(20, Nil()))
+      assertEquals(expected, drop(list, 5))
+
+    @Test
+    def dropNegativeIndex() =
+      val expected = Cons(10, Cons(20, Nil()))
+      assertEquals(expected, drop(list, -1))
+
+    @Test
+    def dropNil() =
+      assertEquals(expectedNil, drop(Nil(), 0))
+    @Test
+    def appendNil() =
+      assertEquals(expectedNil, append(Nil(), Nil()))
+
+    @Test
+    def flatMapNil() =
+      assertEquals(expectedNil, flatMap(Nil[Int]())(el => Cons(el+1, Nil())))
+
+    @Test
+    def mapNil() =
+      assertEquals(expectedNil, map(Nil())(el => Nil[Int]()))
+class Part2MoreListTests:
+  import u02.Modules.Person.*
+  import u02.Modules.Person
+  import u03.homework.Part1MoreOnLists.*
+
+
+  val list = Cons(3, Cons(7, Cons(1, Cons(5, Nil()))))
+
+  @Test
+  def getProfessorCourses() =
+    val persons =
+      Cons(Student("s1", 2022),
+        Cons(Teacher("t1", "c1"),
+          Cons(Teacher("t2", "c2"),
+            Cons(Student("s2", 2023), Nil()))))
+
+    val expected = Cons("c1", Cons("c2", Nil()))
+    assertEquals(expected, getCourses(persons))
+
+  @Test
+  def foldListLeft() =
+    assertEquals(-16, foldLeft(list)(0)(_ - _))
+
+  @Test
+  def foldListRight() =
+    assertEquals(-8, foldRight(list)(0)(_ - _))
+
+/* TODO part 3*/
+class Part3Streams:
+
+  def test() =
+    assertEquals(0, 0)
